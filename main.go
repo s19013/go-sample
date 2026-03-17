1de3ec21298b3dac25f9deaf177a0b6c03f1ff80
s19013 <hideya670@gmail.com>
2026-03-17 16:57:06 +0900

    ポート類をうけとらなくなったので修正

--- main.go
+++ main.go
@@ -13,20 +13,9 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Printf("need port number\n")
-		os.Exit(1)
-	}
-
-	p := os.Args[1]
-	l, err := net.Listen("tcp", ":"+p)
+	err := run(context.Background())
 	if err != nil {
-		log.Fatalf("failed to listen port %s : %v", p, err)
-	}
-
-	err = run(context.Background(), l)
-	if err != nil {
-		log.Printf("failed to terminate server: %v", err)
+		log.Printf("failed to terminated server: %v", err)
 		os.Exit(1)
 	}
 }
