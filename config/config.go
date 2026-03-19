c6db9daf5dac9141b2940d8290abed3b1881e39e
s19013 <hideya670@gmail.com>
2026-03-19 18:40:13 +0900

    データベースに関する環境変数を取得

--- config/config.go
+++ config/config.go
@@ -9,8 +9,13 @@ import (
 type Config struct {
 	// 環境変数（Environment Variables）から設定値を読み込んで Config 構造体に入れる処理
 	// env:"環境変数名" envDefault:"デフォルト値"
-	Env  string `env:"TODO_ENV" envDefault:"dev"`
-	Port int    `env:"PORT" envDefault:"80"`
+	Env        string `env:"TODO_ENV" envDefault:"dev"`
+	Port       int    `env:"PORT" envDefault:"80"`
+	DBHost     string `env:"TODO_DB_HOST" envDefault:"127.0.0.1"`
+	DBPort     int    `env:"TODO_DB_PORT" envDefault:"33306"`
+	DBUser     string `env:"TODO_DB_USER" envDefault:"todo"`
+	DBPassword string `env:"TODO_DB_PASSWORD" envDefault:"todo"`
+	DBName     string `env:"TODO_DB_NAME" envDefault:"todo"`
 }
 
 func New() (*Config, error) {
