616c50326148cf546aedb298f92a271959f7e45f
s19013 <hideya670@gmail.com>
2026-03-23 15:16:01 +0900

    説明追記

--- handler/service.go
+++ handler/service.go
@@ -8,6 +8,8 @@ import (
 
 // リクエストの解釈、レスポンスを組み立てる処理以外をこちらに委譲する
 
+// ソースコードを自動生成するための記述
+//
 //go:generate go run github.com/matryer/moq -out moq_test.go . ListTasksService AddTaskService
 type ListTasksService interface {
 	ListTasks(ctx context.Context) (entity.Tasks, error)
