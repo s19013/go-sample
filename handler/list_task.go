9421aa295c2fb80ad64ac26551e1dc6f0a4afb9e
s19013 <hideya670@gmail.com>
2026-03-23 18:33:07 +0900

    直接handlerの中では直接リポジトリを使わずサービスを使うように修正

--- handler/list_task.go
+++ handler/list_task.go
@@ -3,14 +3,11 @@ package handler
 import (
 	"net/http"
 
-	"github.com/jmoiron/sqlx"
 	"github.com/s19013/go-sample/entity"
-	"github.com/s19013/go-sample/store"
 )
 
 type ListTask struct {
-	DB   *sqlx.DB
-	Repo *store.Repository
+	Service ListTasksService
 }
 
 type task struct {
@@ -23,7 +20,7 @@ func (lt *ListTask) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
 	// 取得
-	tasks, err := lt.Repo.ListTasks(ctx, lt.DB)
+	tasks, err := lt.Service.ListTasks(ctx)
 	if err != nil {
 		RespondJSON(ctx, w, &ErrResponse{
 			Message: err.Error(),
