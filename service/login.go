c433bf9c39da22327cc102031367c18f4f25febd
s19013 <hideya670@gmail.com>
2026-03-29 16:47:01 +0900

    コメント追記

--- service/login.go
+++ service/login.go
@@ -13,14 +13,20 @@ type Login struct {
 	TokenGenerator TokenGenerator
 }
 
+// ログイン情報の検証とアクセストークンの生成を行う
 func (l *Login) Login(ctx context.Context, name, pw string) (string, error) {
+	// ユーザー情報取得
 	u, err := l.Repo.GetUser(ctx, l.DB, name)
 	if err != nil {
 		return "", fmt.Errorf("failed to list: %w", err)
 	}
+
+	// パスワード検証
 	if err := u.ComparePassword(pw); err != nil {
 		return "", fmt.Errorf("wrong password: %w", err)
 	}
+
+	// トークン生成
 	jwt, err := l.TokenGenerator.GenerateToken(ctx, *u)
 	if err != nil {
 		return "", fmt.Errorf("failed to generate JWT: %w", err)
