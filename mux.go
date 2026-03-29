af001289e1c03e5e0fb3255219bed086806ffedb
s19013 <hideya670@gmail.com>
2026-03-29 16:55:24 +0900

    エンドポイント追加

--- mux.go
+++ mux.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-playground/validator/v10"
+	"github.com/s19013/go-sample/auth"
 	"github.com/s19013/go-sample/clock"
 	"github.com/s19013/go-sample/config"
 	"github.com/s19013/go-sample/handler"
@@ -28,8 +29,37 @@ func NewMux(ctx context.Context, cfg *config.Config) (http.Handler, func(), erro
 		return nil, cleanup, err
 	}
 
-	r := store.Repository{Clocker: clock.RealClocker{}}
+	// 時刻管理 + Repository
+	clocker := clock.RealClocker{}
+	r := store.Repository{Clocker: clocker}
 
+	// KVS作成
+	rcli, err := store.NewKVS(ctx, cfg)
+	if err != nil {
+		return nil, cleanup, err
+	}
+
+	// JWT作成
+	jwter, err := auth.NewJWTer(rcli, clocker)
+	if err != nil {
+		return nil, cleanup, err
+	}
+
+	ru := &handler.RegisterUser{
+		Service:   &service.RegisterUser{DB: db, Repo: &r},
+		Validator: v,
+	}
+	mux.Post("/register", ru.ServeHTTP)
+
+	l := &handler.Login{
+		Service: &service.Login{
+			DB:             db,
+			Repo:           &r,
+			TokenGenerator: jwter,
+		},
+		Validator: v,
+	}
+	mux.Post("/login", l.ServeHTTP)
 	at := &handler.AddTask{
 		Service:   &service.AddTask{DB: db, Repo: &r},
 		Validator: v,
@@ -40,12 +70,6 @@ func NewMux(ctx context.Context, cfg *config.Config) (http.Handler, func(), erro
 		Service: &service.ListTask{DB: db, Repo: &r},
 	}
 	mux.Get("/tasks", lt.ServeHTTP)
-
-	ru := &handler.RegisterUser{
-		Service:   &service.RegisterUser{DB: db, Repo: &r},
-		Validator: v,
-	}
-	mux.Post("/register", ru.ServeHTTP)
 
 	return mux, cleanup, nil
 }
